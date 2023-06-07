beb720da7dcace1b52f4c5a30d91c76a53e7da37
Sabir Mondal <sabir.mondal@cloudcraftz.io>
2023-06-07 12:04:35 +0530

    DEV: UI developed

---
+++ lib/views/widgets/app_button.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  const AppButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
